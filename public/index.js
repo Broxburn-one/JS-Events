7b57f2952337759cda7cb4d67d2c2ecb24583403
Borxburn-One <broxburn001@yahoo.co.uk>
2016-03-28 19:51:49 +0100

    first init

---
+++ public/index.js
@@ -0,0 +1,82 @@
+var init = function(){
+  // console.log('app started');
+
+  var button = document.getElementById('add-button');
+  var form = document.getElementById('quote-form');
+
+  var palaver = document.getElementById('quote');
+  var author = document.getElementById('author');
+
+  var quotes = [
+    {'text': "Visual Basic is the way forward, I don't know why we are doing JavaScript.", 
+    'author': 'Jay Chetty'},
+    {'text': 'The only CSS you need to know is background-color: tomato',
+     'author': 'Rick'},
+     {'text': 'Are those hand-cut artisan crisps?',
+     'author': 'Keith'},
+     {'text': "Scaffolding is nothing but a fiery hell.",
+     'author': 'Valerie'}
+    ];
+
+    var li;
+    var ul = document.getElementById('quote-list');
+
+    for (quote of quotes){
+       output = quote.text + ' : ' + '<span>' + quote.author + '</span>';
+      
+      li = document.createElement('li');
+      
+      li.innerHTML = output;
+      ul.appendChild(li);
+    }
+
+
+  var handleClick = function() {
+    var input = palaver.value + ' : ' + '<span>' + author.value + '</span>';
+    li = document.createElement('li');
+    li.innerHTML = input;
+    ul.appendChild(li);
+  }
+
+//--
+    function getEventTarget(e) {
+        e = e || window.event;
+        return e.target || e.srcElement; 
+    }
+    
+    ul.onclick = function(event) {
+        var target = getEventTarget(event);
+        target.parentNode.removeChild(target);
+       // alert(target.innerHTML);
+    };
+
+//--
+
+
+
+
+
+  form.onsubmit = function(event){
+    event.preventDefault();
+    handleClick();
+  } // form
+
+
+}; // init
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+window.onload = init;
